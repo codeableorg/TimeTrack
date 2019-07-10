07b8041cb092386e6c36050f07de184000b0bf78
Jona <jcmendozar10@gmail.com>
2019-07-10 18:04:06 -0500

    Create fake data for tests

--- rails-time-track/db/seeds.rb
+++ rails-time-track/db/seeds.rb
@@ -7,42 +7,70 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-include faker
 
-10.times do
-  CreateUsers.create(
+3.times do
+  User.create(
     name: Faker::Name.name,
     email: Faker::Internet.email,
-    role: Faker::Job.position.role
-    rate: Faker::Number.number(3).rate,
+    # role: Faker::Job.position,
+    role: "Owner",
+    rate: Faker::Number.between(1000,2000)
   )
 end
 
-10.times do
-  CreateProjects.create(
+3.times do
+  User.create(
     name: Faker::Name.name,
-    client: Faker::Company.name.client,
-    category: Faker::Company.industry.category,
-    start: Faker::Date.between(365.days.ago, Date.today).start,
-    projectedEnd: Faker::Date.between(365.days.ago, Date.today).projectedEnd,
+    email: Faker::Internet.email,
+    role: "Manager",
+    rate: Faker::Number.between(1000,2000)
+  )
+end
+
+4.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    role: "Analist",
+    rate: Faker::Number.between(1000,2000)
+  )
+end
+
+
+
+10.times do
+  Project.create(
+    name: Faker::Name.name,
+    client: Faker::Company.name,
+    category: Faker::Company.industry,
+    start_date: Faker::Date.between(365.days.ago, Date.today),
+    end_date: Faker::Date.between(50.days.ago, Date.today),
     closed: Faker::Boolean.boolean,
-    estimated_cost: Faker::Number.number(5).estimated_cost,
-    real_cost: Faker::Number.number(5).real_cost
+    estimated_cost: Faker::Number.number(7),
+    real_cost: Faker::Number.number(7)
   )
 end
 
 10.times do
-  CreateProjectMembers(
-    name: Faker::Name.name,
-    name: Faker::Name.name,
-    estimated_cost: Faker::Number.number(5).estimated_cost,
-    real_cost: Faker::Number.number(5).real_cost
+  ProjectMember.create(
+    # name: Faker::Name.name,
+    estimated_cost: Faker::Number.number(7),
+    real_cost: Faker::Number.number(7)
   )
 end
 
 10.times do
-  CreateDailyLogs(
-    date: 
+  DailyLog.create(
+    date: Faker::Date.between(100.days.ago, Date.today),
+    amount: Faker::Number.number(6),
+  )
+end
+
+10.times do
+  WeeklyProjectReport.create(
+    week: Faker::Number.between(1, 52),
+    estimated_cost: Faker::Number.number(7),
+    real_cost: Faker::Number.number(7)
   )
 end
 
