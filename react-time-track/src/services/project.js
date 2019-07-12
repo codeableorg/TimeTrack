0793d60affa80f7711be38c0a95f3b52063791ea
paulotijero <paulotijero@gmail.com>
2019-07-12 00:06:23 -0500

    Add connection between bach and frontend

--- react-time-track/src/services/project.js
+++ react-time-track/src/services/project.js
@@ -1,4 +1,5 @@
 const API_ALL_PROJECTS = "http://localhost:3000/api/projects";
+const API_CLOSED_PROJECTS = "http://localhost:3000/api/histories";
 
 async function listProjects() {
   const list = await fetch(API_ALL_PROJECTS, {
@@ -12,7 +13,7 @@ async function listProjects() {
 }
 
 async function closedProjects() {
-  const list = await fetch(API_ALL_PROJECTS, {
+  const list = await fetch(API_CLOSED_PROJECTS, {
     method: "GET",
     credentials: "include",
     headers: {
