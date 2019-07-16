5df6bee37c285701e5b0216faf1393e1c878fb57
Diego Torres <diegotc86@gmail.com>
2019-07-16 10:05:50 -0500

    Add css grid on index

--- react-time-track/src/views/all-projects.js
+++ react-time-track/src/views/all-projects.js
@@ -5,18 +5,20 @@ import { jsx } from "@emotion/core";
 import { Button, Subtitle } from "../components/ui";
 import { Center } from "../components/helpers";
 import ListProjects from "../components/list-projects";
-import NavBar from "../components/navbar";
 
 function AllProjects() {
   return (
-    <>
-      <NavBar />
+    <div
+      css={{
+        gridArea: "main"
+      }}
+    >
       <Subtitle>All Projects:</Subtitle>
       <ListProjects />
       <Center>
         <Button>New Project</Button>
       </Center>
-    </>
+    </div>
   );
 }
 
