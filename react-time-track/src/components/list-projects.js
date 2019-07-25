89cb8890bd3441bf024c131671a8203ad435c01f
Carlos A <cayala.w@gmail.com>
2019-07-24 19:59:14 -0500

    Calculate project status and set styles

--- react-time-track/src/components/list-projects.js
+++ react-time-track/src/components/list-projects.js
@@ -21,6 +21,52 @@ function calculateProgress(real, estimated) {
   return ((parseInt(real) / parseInt(estimated)) * 100).toFixed(0) + "%";
 }
 
+function calculateRisk(project) {
+  let riskValue = [];
+  const projectDays =
+    (Date.parse(project.end_date) + 1 - Date.parse(project.start_date)) /
+    (1000 * 60 * 60 * 24); // calculate duration of the project
+
+  const usedDays =
+    (Date.now() - Date.parse(project.start_date)) / (1000 * 60 * 60 * 24); // calculate used days
+
+  const currentEstimatedCost =
+    (project.estimated_cost * usedDays) / projectDays;
+
+  const currentRealCost = project.real_cost;
+
+  console.log(
+    `Días totales del proyecto id ${project.id}: ${Math.abs(
+      Math.round(projectDays)
+    )}, inicio: ${project.start_date} final: ${project.end_date}`
+  ); // show available days of project, start and end date
+
+  console.log(
+    `Días usados ${project.id}: ${Math.abs(Math.round(usedDays))}, inicio: ${
+      project.start_date
+    } actual: ${new Date()}`
+  ); // show used days of project, start and current date
+
+  console.log(
+    `Proyecto: ${project.id}: 
+    Costo estimado proyecto: ${project.estimated_cost}, 
+    Costo real proyecto: ${project.real_cost}, 
+    costo estimado actual: ${currentEstimatedCost}; 
+    costo real actual: ${currentRealCost},
+    estado: ${currentRealCost / currentEstimatedCost}`
+  );
+
+  if (currentRealCost / currentEstimatedCost > 1) riskValue = red;
+  else if (
+    currentRealCost / currentEstimatedCost < 1 &&
+    currentRealCost / currentEstimatedCost > 0.9
+  )
+    riskValue = ambar;
+  else if (currentRealCost / currentEstimatedCost < 0.9) riskValue = green;
+  // console.log("Estilos circulo: ", riskValue);
+  return riskValue;
+}
+
 function ListProjects() {
   const [projects, setProjects] = React.useState([]);
 
@@ -37,9 +83,11 @@ function ListProjects() {
           return (
             <Card styles={card} key={project.id} role="listitem">
               <Link to={`/projects/${project.id}`}>
+                {/* {console.log("project data: ", project)} */}
                 <span>{project.name}</span>
               </Link>
-              <Circle>
+              <Circle styles={calculateRisk(project)}>
+                {/* <Circle styles={red}> */}
                 {calculateProgress(project.real_cost, project.estimated_cost)}
               </Circle>
             </Card>
