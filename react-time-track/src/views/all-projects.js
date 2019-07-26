9583ddf9d8cc5e054da0192524a402a138acee0f
Diego Torres <diegotc86@gmail.com>
2019-07-26 08:52:38 -0500

    Revert "Feature create new project"

--- react-time-track/src/views/all-projects.js
+++ react-time-track/src/views/all-projects.js
@@ -1,23 +1,18 @@
 /** @jsx jsx */
 import React from "react";
 import { jsx } from "@emotion/core";
-import { navigate } from "@reach/router";
 
 import { Button, Subtitle } from "../components/ui";
 import { Center } from "../components/helpers";
 import ListProjects from "../components/list-projects";
 
 function AllProjects() {
-  function handleClick() {
-    navigate("/create-project");
-  }
-
   return (
     <div>
       <Subtitle>All Projects:</Subtitle>
       <ListProjects />
       <Center>
-        <Button onClick={handleClick}>New Project</Button>
+        <Button>New Project</Button>
       </Center>
     </div>
   );
