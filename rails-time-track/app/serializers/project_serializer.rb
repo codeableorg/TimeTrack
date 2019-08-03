4601e51a32641dab816a050ed1ff4a211e0594e9
Carlos A <cayala.w@gmail.com>
2019-08-03 18:17:54 -0500

    Updating with develop branch

--- rails-time-track/app/serializers/project_serializer.rb
+++ rails-time-track/app/serializers/project_serializer.rb
@@ -5,10 +5,6 @@ class ProjectSerializer < ActiveModel::Serializer
     self.object.users.select("users.id, users.name, users.role")
   end
 
-  def daily
-    self.object.daily_logs.select("daily_logs.id, daily_logs.project_member_id, daily_logs.date, daily_logs.amount")
-  end
-
 
   def weekly
     self.object.weekly_project_reports.select("weekly_project_reports.id, weekly_project_reports.week, weekly_project_reports.estimated_cost, weekly_project_reports.real_cost")
