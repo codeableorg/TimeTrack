3b4ee7f19b31588c4b20ddb9cff799e33845388a
Carlos A <cayala.w@gmail.com>
2019-08-04 23:34:23 -0500

    API returns necessary filed to calculate project progress

--- rails-time-track/app/serializers/project_serializer.rb
+++ rails-time-track/app/serializers/project_serializer.rb
@@ -1,11 +1,16 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :client, :category, :start_date, :end_date, :closed, :estimated_cost, :real_cost, :members, :weekly, :user_detail
+  attributes :id, :name, :client, :category, :start_date, :end_date, :closed, :estimated_cost, :real_cost, :members, :weekly, :user_detail#, :daily
 
   def members
-    self.object.users.select("users.id, users.name, users.role")
+    Project.joins(:users, :project_members).select("users.id, users.name, users.role, project_members.estimated_cost, project_members.real_cost")
+    # self.object.users.select("users.id, users.name, users.role")
+    # Project.joins(:users, :project_members).select("users.id, users.name, users.role, project_members.estimated_cost, project_members.real_cost")
   end
-
-
+=begin
+  def daily
+    self.object.daily_logs.select("daily_logs.id, daily_logs.project_member_id, daily_logs.date, daily_logs.amount")
+  end
+=end
   def weekly
     self.object.weekly_project_reports.select("weekly_project_reports.id, weekly_project_reports.week, weekly_project_reports.estimated_cost, weekly_project_reports.real_cost")
   end
