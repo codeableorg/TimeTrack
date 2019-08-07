e02474d061079137a038cba989ac0a279cf2b823
Diego Torres <diegotc86@gmail.com>
2019-08-07 01:00:24 -0500

    Refactor Daily Log creation process

--- rails-time-track/app/controllers/api/daily_logs_controller.rb
+++ rails-time-track/app/controllers/api/daily_logs_controller.rb
@@ -6,16 +6,28 @@ module Api
     end
 
     def create
-      daily_log = DailyLog.new(new_daily_log_params)
-      if daily_log.save
-        render json: daily_log, status: 201
-      else
-        render_error_message(daily_log.errors.messages, 400)
+      if params[:data].all? do |daily|
+        user = daily[:user_id]
+        date = daily[:date]
+        project_member = ProjectMember.find_by(project_id: daily[:project_id], user_id: daily[:user_id])
+        
+        found = DailyLog.find_by(project_member: project_member.id, date: daily[:date])
+        unless found 
+          daily_log = DailyLog.new(
+            project_member: project_member,
+            date: daily[:date],
+            amount: daily[:amount]
+          )
+          daily_log.save
+        else
+          false
+        end
       end
-    end
-    
-    def new_daily_log_params
-      params.permit(:project_member_id, :date, :amount)
+      render json: { message: "Daily Logs saved" }, status: :created
+      else
+        render_errors("Daily Log already exists", :unprocessable_entity)
+      end
+      
     end
 
     rescue_from ActiveRecord::RecordNotFound do |e|
